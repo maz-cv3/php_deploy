36d2c4694d3d153cf79be43bc470944d9dc2e0b4
CV3 <cv3@192.168.1.54>
2023-03-02 21:56:40 +0530

    new version deployed

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "version v1.0.1! <br>";
+echo "version v1.0.2! <br>";
 echo "Hello, World from Docker! <br>";
 echo "Hello ECS! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
