a17baad62081169108d7bb620f247f2c2d49d853
maz-cv3 <119608706+maz-cv3@users.noreply.github.com>
2023-05-17 13:14:48 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,9 +1,6 @@
 <?php
 
-require_once 'vendor/autoload.php';
-\Sentry\init(['dsn' => 'http://dc5f73a5d03e4c828bf23a81a832d462@10.0.3.77:9000/1', 'traces_sample_rate' => 1.0,
-        'profiles_sample_rate' => 1.0
- ]);
+
 
 echo "version v1.0.2! <br>";
 echo "Hello, World from Docker! <br>";
